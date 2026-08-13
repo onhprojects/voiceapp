e401698739f5c2045546804a0af84b5de59c322b
onhprojects <onhprojects@outlook.com>
2026-08-13 20:16:14 +0000

    Redirect new users to dashboard instead of email verification

--- nextjs/src/app/auth/register/page.tsx
+++ nextjs/src/app/auth/register/page.tsx
@@ -37,7 +37,7 @@ export default function RegisterPage() {
 
             if (error) throw error;
 
-            router.push('/auth/verify-email');
+            router.push('/app');
         } catch (err: Error | unknown) {
             if(err instanceof Error) {
                 setError(err.message);
